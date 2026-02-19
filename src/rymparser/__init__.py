fe20c29858954622ee299434b2defc417d8648b5
Yevhenii <21296902+99994433552@users.noreply.github.com>
2026-02-19 21:33:12 +0200

    refactor: remove monolith, wire up package exports and CLI

--- src/rymparser/__init__.py
+++ src/rymparser/__init__.py
@@ -1,4 +1,20 @@
-"""Parse RYM list pages into 'Artist - Album (Year)' format.
+"""Parse RYM list pages for Soulseek search."""
 
-Intended for Soulseek search.
-"""
+from rymparser.browser import FetchError, fetch_all_pages
+from rymparser.config import ScraperConfig
+from rymparser.models import Album
+from rymparser.parser import (
+    extract_slug,
+    find_next_page_url,
+    parse_page,
+)
+
+__all__ = [
+    "Album",
+    "FetchError",
+    "ScraperConfig",
+    "extract_slug",
+    "fetch_all_pages",
+    "find_next_page_url",
+    "parse_page",
+]
