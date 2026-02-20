44dbe0950a6a7ee88827bc25fea2a122e4fa3692
Yevhenii <21296902+99994433552@users.noreply.github.com>
2026-02-20 13:05:42 +0200

    feat: export artist page parsing functions

--- src/rymparser/__init__.py
+++ src/rymparser/__init__.py
@@ -1,8 +1,17 @@
 """Parse RYM list pages for Soulseek search."""
 
-from rymparser.browser import FetchError, fetch_all_pages
+from rymparser.artist_parser import (
+    DEFAULT_TYPES,
+    extract_artist_slug,
+    parse_artist_page,
+)
+from rymparser.browser import (
+    FetchError,
+    fetch_all_pages,
+    fetch_artist_page,
+)
 from rymparser.config import ScraperConfig
-from rymparser.models import Album
+from rymparser.models import Album, ReleaseType
 from rymparser.organizer import wait_and_organize
 from rymparser.parser import (
     extract_slug,
@@ -19,18 +28,23 @@ from rymparser.settings import AppSettings, load_settings
 from rymparser.slskd_client import SlskdError
 
 __all__ = [
+    "DEFAULT_TYPES",
     "Album",
     "AlbumSearchResult",
     "AppSettings",
     "FetchError",
+    "ReleaseType",
     "ScraperConfig",
     "SlskdError",
     "build_query",
+    "extract_artist_slug",
     "extract_slug",
     "fetch_all_pages",
+    "fetch_artist_page",
     "filter_responses",
     "find_next_page_url",
     "load_settings",
+    "parse_artist_page",
     "parse_page",
     "rank_results",
     "wait_and_organize",
